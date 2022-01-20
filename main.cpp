36d023f096cf97407bc3b49644357455469ee026
Vasilisa2001 <90385212+Vasilisa2001@users.noreply.github.com>
2022-01-21 01:35:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,69 +2,68 @@
 #include <string>
 using namespace std;
 
+unsigned short personHeight;
+ 
 unsigned short ReadPersonAge() {
+    unsigned short age;
     cout << "age: ";
-    unsigned short in;
-    cin >> in;
-    return in;
-};
-
+    cin >> age;
+    return age;
+}
+ 
 string ReadPersonName() {
+    string name;
     cout << "name: ";
-    string in;
-    cin >> in;
-    return in;
-};
-
-unsigned short global_height;
-
+    cin >> name;
+    return name;
+}
+ 
 void ReadPersonHeight() {
     cout << "height: ";
-    cin >> global_height;
-};
-
-void ReadPersonWeight(unsigned short& out) {
+    cin >> personHeight;
+}
+ 
+void ReadPersonWeight(unsigned short& weight) {
     cout << "weight: ";
-    cin >> out;
-};
-
-void ReadPersonSalary(double* out) {
+    cin >> weight;
+}
+ 
+void ReadPersonSalary(double* salary) {
     cout << "salary: ";
-    cin >> *out;
-};
-
+    cin >> *salary;
+}
+ 
 void ReadPersonData(string& name, unsigned short& age, double& salary) {
     name = ReadPersonName();
     age = ReadPersonAge();
     ReadPersonSalary(&salary);
-};
+}
+
 
 void ReadPersonData(string& name, unsigned short& age, unsigned short& height, unsigned short& weight) {
     name = ReadPersonName();
     age = ReadPersonAge();
     ReadPersonHeight();
-    height = global_height;
-    unsigned short w;
-    ReadPersonWeight(w);
-    weight = w;
-};
+    height = personHeight;
+    ReadPersonWeight(weight);
+}
 
-void WritePersonData(const string& name, const unsigned short age, const string& height = "", const string& weight = "", const string& salary = "") {
-    cout << "   Name   " << name << endl;
-    cout << "   Age    " << age << endl;
-    cout << "  Height " << height << endl;
-    cout << "  Weight " << weight << endl;
-    cout << "  Salary " << salary << endl;
-};
 
-int main(int argc, char** argv) {
+void WritePersonData(unsigned short age, string name, const string& height = "", const string& weight = "", const string& salary = "") {
+    cout << name << endl;
+    cout << age << endl;
+    cout << height << endl;
+    cout << weight << endl;
+    cout << salary << endl;
+}
+
+int main() {
     string name;
     unsigned short age, height, weight;
     double salary;
     ReadPersonData(name, age, salary);
-    cout << "Result: \n";
-    WritePersonData(name, age, "", "", to_string(salary));
+    WritePersonData(age, name, "", "", to_string(salary));
     ReadPersonData(name, age, height, weight);
-    cout << "Result: \n";
-    WritePersonData(name, age, to_string(height), to_string(weight));
-};
+    WritePersonData(age, name, to_string(height), to_string(weight));
+    return 0;
+}
